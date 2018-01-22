7457c3b52f09f53467a3f6f7b071069322948894
Tim Vu <timbim8996@gmail.com>
2018-01-21 16:55:53 -0800

    added loop into drive straight DD 1-21-2018

--- Team-2637-PowerUp/src/autonomous/DriveStraight.java
+++ Team-2637-PowerUp/src/autonomous/DriveStraight.java
@@ -4,10 +4,10 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import robot.CatzRobotMap;
 public class DriveStraight
 {
-	CatzRobotMap robotmap;
+	CatzRobotMap robotMap;
 	void EncoderStraightDrive(double speed, double distance, double sampleTime,double timeout)
 	{
-		robotmap = CatzRobotMap.getInstance();
+		robotMap = CatzRobotMap.getInstance();
 		int loopCount        = 0;
 		double encoderIssues = 0;
 		int dbgCount1        = 0;
@@ -23,46 +23,47 @@ public class DriveStraight
 		double encoderCheckNumber;
 		double lastEncoderValue = 0;
 
-		robotmap.navx.reset();
-		robotmap.wheelEncoder.reset();
+		robotMap.navx.reset();
+		robotMap.wheelEncoder.reset();
 
-		robotmap.timer.reset(CatzConstants.FUNCTION_TIMER);
-		robotmap.timer.start(CatzConstants.FUNCTION_TIMER);
-
-		currentAngle = robotmap.navx.getAngle();
-
-		deltaAngle = currentAngle-previousAngle;
-
-		derivative = deltaAngle/deltaTime;
-
-		robotmap.drive.tankDrive(speed, CatzConstants.straightkP*currentAngle + CatzConstants.straightkD*derivative);
-
-		previousAngle = currentAngle;
-
-		if (robotmap.timer.get(CatzConstants.FUNCTION_TIMER) > timeout)
-			done = true;
-
-			/***********************************************
-			*encoderCheckNumber = wheelEncoder.Get();
-			*if(lastEncoderValue==encoderCheckNumber)
-			*	encoderIssues++;
-			*lastEncoderValue=encoderCheckNumber;
-			*loopCount++;
-			*dbgCount1++;
-			************************************************/
-
-		dbgCount1++;
-		if (dbgCount1== CatzConstants.VAR_1_BUFFER_SIZE)
-			dbgCount1=0;
-
+		robotMap.timer.reset(CatzConstants.FUNCTION_TIMER);
+		robotMap.timer.start(CatzConstants.FUNCTION_TIMER);
+		while(Math.abs(robotMap.wheelEncoder.getDistance()) < distance && done != true)
+		{
+			currentAngle = robotMap.navx.getAngle();
+	
+			deltaAngle = currentAngle-previousAngle;
+	
+			derivative = deltaAngle/deltaTime;
+	
+			robotMap.drive.tankDrive(speed, CatzConstants.straightkP*currentAngle + CatzConstants.straightkD*derivative);
+	
+			previousAngle = currentAngle;
+	
+			if (robotMap.timer.get(CatzConstants.FUNCTION_TIMER) > timeout)
+				done = true;
+	
+				/***********************************************
+				*encoderCheckNumber = wheelEncoder.Get();
+				*if(lastEncoderValue==encoderCheckNumber)
+				*	encoderIssues++;
+				*lastEncoderValue=encoderCheckNumber;
+				*loopCount++;
+				*dbgCount1++;
+				************************************************/
+	
+			dbgCount1++;
+			if (dbgCount1== CatzConstants.VAR_1_BUFFER_SIZE)
+				dbgCount1=0;
+		}
 		if(speed<0)
-			robotmap.drive.tankDrive(.43,0);
+			robotMap.drive.tankDrive(.43,0);
 		else
-			robotmap.drive.tankDrive(-.43,0);
-		robotmap.drive.tankDrive(0,0);
-		robotmap.timer.stop(CatzConstants.FUNCTION_TIMER);
+			robotMap.drive.tankDrive(-.43,0);
+		robotMap.drive.tankDrive(0,0);
+		robotMap.timer.stop(CatzConstants.FUNCTION_TIMER);
 
-		SmartDashboard.putNumber("Function timer value", robotmap.timer.get(CatzConstants.FUNCTION_TIMER));
+		SmartDashboard.putNumber("Function timer value", robotMap.timer.get(CatzConstants.FUNCTION_TIMER));
 		SmartDashboard.putNumber("encoderCheck", encoderIssues);
 		SmartDashboard.putNumber("drive straight loop count", loopCount);
 	}	
