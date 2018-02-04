a0f8bc06113e1b52463b45416120e8a72d8cd42a
Derek Duenas <dereksduenas@gmail.com>
2018-02-04 11:59:15 -0800

    added headers to climber and lift classes DD 2-4-2018

--- Team-2637-PowerUp/src/mechanisms/CatzClimber.java
+++ Team-2637-PowerUp/src/mechanisms/CatzClimber.java
@@ -1,8 +1,15 @@
 package mechanisms;
 
 import org.usfirst.frc.team2637.robot.CatzRobotMap;
-
 import constants.CatzConstants;
+
+/*
+ *  Author : Derek Duenas
+ *  Last Revised : 2-4-2018 DD
+ *  Added functionality to be able to climb
+ *  Methods : climb
+ *  Functionality : Climb
+ */
 
 public class CatzClimber 
 {
@@ -10,7 +17,25 @@ public class CatzClimber
 	public void climb()
 	{
 		instance = CatzRobotMap.getInstance();
-		instance.climber.set(CatzConstants.CLIMB_SPEED);
-		instance.climber2.set(CatzConstants.CLIMB_SPEED);
+		if(instance.joy.getButtonThree())
+		{
+			instance.climber.set(CatzConstants.CLIMB_SPEED);
+			instance.climber2.set(CatzConstants.CLIMB_SPEED);
+		}
+		else
+		{
+			instance.climber.set(CatzConstants.ZERO_DOUBLE);
+			instance.climber2.set(CatzConstants.ZERO_DOUBLE);
+		}
+		if(instance.joy.getButtonFour())
+		{
+			instance.climber.set(-CatzConstants.CLIMB_SPEED);
+			instance.climber2.set(-CatzConstants.CLIMB_SPEED);
+		}
+		else
+		{
+			instance.climber.set(CatzConstants.ZERO_DOUBLE);
+			instance.climber2.set(CatzConstants.ZERO_DOUBLE);
+		}
 	}
 }
--- Team-2637-PowerUp/src/mechanisms/CatzLift.java
+++ Team-2637-PowerUp/src/mechanisms/CatzLift.java
@@ -1,8 +1,15 @@
 package mechanisms;
 
 import org.usfirst.frc.team2637.robot.CatzRobotMap;
-
 import constants.CatzConstants;
+
+/*
+ *  Author : Derek Duenas
+ *  Last Revised : 2-4-2018 DD
+ *  Added functionality to move the lift up and down
+ *  Methods : lift
+ *  Functionality : Move the lift up and down
+ */
 
 public class CatzLift 
 {
