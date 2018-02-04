dea63e3ac317873ffa9deffedc122b38665446c3
Derek Duenas <dereksduenas@gmail.com>
2018-02-03 16:32:39 -0800

    changed ports and added new components DD 2-3-2018

--- Team-2637-PowerUp/src/org/usfirst/frc/team2637/robot/CatzRobotMap.java
+++ Team-2637-PowerUp/src/org/usfirst/frc/team2637/robot/CatzRobotMap.java
@@ -4,10 +4,12 @@ import com.kauailabs.navx.frc.AHRS;
 import components.CatzCANTalonSRX;
 import components.CatzDrive;
 import components.CatzJoystick;
+import components.CatzSpark;
 import components.CatzXboxController;
 import constants.CatzConstants;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -23,7 +25,15 @@ public class CatzRobotMap
 	public CatzCANTalonSRX fLeft;
 	public CatzCANTalonSRX rLeft;
 	
-	public Encoder wheelEncoder;
+	public Encoder wheelEncoderR;
+	public Encoder wheelEncoderL;
+	
+	public CatzSpark lifterR;
+	public CatzSpark lifterL;
+	
+	public CatzSpark intake;
+	public CatzSpark intake2;
+	
 	public AHRS navx;
 	
 	public Timer timer;
@@ -36,6 +46,9 @@ public class CatzRobotMap
 	
 	public Thread m_visionThread;
 	
+	public Solenoid intakeOpen;
+	public Solenoid intakeDeploy;
+	
 	//public static CatzLogger logger;
 	
 	//public  Encoder rightWheelEncoder
@@ -46,14 +59,15 @@ public class CatzRobotMap
 	private CatzRobotMap() 
 	{
 		
-		fRight = new CatzCANTalonSRX(CatzConstants.PORT_4);
+		fRight = new CatzCANTalonSRX(CatzConstants.PORT_6);
 		rRight = new CatzCANTalonSRX(CatzConstants.PORT_5);
-		fLeft = new CatzCANTalonSRX(CatzConstants.PORT_0);
-		rLeft = new CatzCANTalonSRX(CatzConstants.PORT_1);
+		fLeft = new CatzCANTalonSRX(CatzConstants.PORT_1);
+		rLeft = new CatzCANTalonSRX(CatzConstants.PORT_2);
 		
 		navx = new AHRS(SerialPort.Port.kMXP);
 		
-		wheelEncoder = new Encoder(CatzConstants.DIO_PORT6, CatzConstants.DIO_PORT7, false, Encoder.EncodingType.k2X);
+		wheelEncoderR = new Encoder(CatzConstants.DIO_PORT2,CatzConstants.DIO_PORT3, false, Encoder.EncodingType.k2X);
+		wheelEncoderL = new Encoder(CatzConstants.DIO_PORT0,CatzConstants.DIO_PORT1,false,Encoder.EncodingType.k2X);
 		
 		timer = new Timer();
 		
@@ -68,6 +82,14 @@ public class CatzRobotMap
 		
 		m_chooser = new SendableChooser<>();
 		
+		lifterR = new CatzSpark(CatzConstants.PWM_PORT_1);
+		lifterL = new CatzSpark(CatzConstants.PWM_PORT_0);
+		
+		intake = new CatzSpark(CatzConstants.PWM_PORT_2);
+		intake2 = new CatzSpark(CatzConstants.PWM_PORT_3);
+		
+		intakeOpen = new Solenoid(CatzConstants.PCM_PORT_0);
+		intakeDeploy = new Solenoid(CatzConstants.PCM_PORT_1);
 		//logger = new CatzLogger();
 	}
 	public static CatzRobotMap getInstance()
