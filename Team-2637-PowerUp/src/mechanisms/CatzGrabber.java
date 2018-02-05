7ca4005d0d1acd180a3943d6866c69aa8f5c79ca
tim Vu <timbim8996@gmail.com>
2018-02-04 16:52:38 -0800

    adding grabber code TV 2/4/18

--- Team-2637-PowerUp/src/mechanisms/CatzGrabber.java
+++ Team-2637-PowerUp/src/mechanisms/CatzGrabber.java
@@ -1,6 +1,63 @@
 package mechanisms;
 
-public class CatzGrabber 
-{
+import org.usfirst.frc.team2637.robot.CatzRobotMap;
+
+import components.CatzXboxController;
+
+public class CatzGrabber {
+
+	CatzRobotMap instance;
+
+	public void openFlap() {
+
+		instance = CatzRobotMap.getInstance();
+		boolean toggleSol = false;
+
+		if (instance.xbox.getLeftBumper() == true) {
+			toggleSol = true;
+
+			if (toggleSol == true)
+				instance.intakeOpen.set(true);
+
+			else
+				instance.intakeOpen.set(false);
+		}
+
+	}
 	
+	
+	public void intakeCube()
+	{
+		instance = CatzRobotMap.getInstance();
+		
+		if(instance.xbox.getRightBumper() == true)
+		{
+			instance.intake.setSpeed(0.7);
+			instance.intake2.setSpeed(0.7);
+		}
+		else
+		{
+			instance.intake.setSpeed(0);
+			instance.intake2.setSpeed(0);
+		}	
+	} 
+	
+	public void retractIntake()
+	{
+	
+		instance = CatzRobotMap.getInstance();
+		
+		if(instance.xbox.getAButton() == true)
+			instance.intakeDeploy.set(false);
+		
+	}
+	
+	public void deployIntake()
+	{
+		
+		instance = CatzRobotMap.getInstance();
+		
+		if(instance.xbox.getBButton() == true)
+			instance.intakeDeploy.set(true);
+	}
 }
