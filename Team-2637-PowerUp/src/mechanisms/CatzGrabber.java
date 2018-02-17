b10f34c22fdc75190fa7d4ad4f0c950b86ae4935
Derek Duenas <dereksduenas@gmail.com>
2018-02-17 10:52:17 -0800

    added open and close grabber to teleop DD 2-17-18

--- Team-2637-PowerUp/src/mechanisms/CatzGrabber.java
+++ Team-2637-PowerUp/src/mechanisms/CatzGrabber.java
@@ -16,52 +16,41 @@ package mechanisms;
 import org.usfirst.frc.team2637.robot.CatzRobotMap;
 import constants.CatzConstants;
 
-public class CatzGrabber {
+public class CatzGrabber 
+{
+	CatzRobotMap instance = CatzRobotMap.getInstance();
 
-	CatzRobotMap instance;
-
-	public void forearmOpen() {
-
-		CatzConstants.forearmOpen = true;
+	public void forearmOpen() 
+	{
 		instance.intakeForearm.set(CatzConstants.forearmOpen);
-
 	}
 
-	public void forearmClose() {
-		CatzConstants.forearmOpen = false;
+	public void forearmClose() 
+	{
 		instance.intakeForearm.set(CatzConstants.forearmOpen);
-
 	}
 
-	public void intakeCube() { // meant to be used with right trigger
+	public void intakeCube() 
+	{
 		instance.intakeRight.setSpeed(CatzConstants.INTAKE_SPEED);
 		instance.intakeLeft.setSpeed(-CatzConstants.INTAKE_SPEED);
-
 	}
 
-	public void launchCube() { // meant to be used with left trigger
-
+	public void launchCube() 
+	{
 		instance.intakeRight.setSpeed(-CatzConstants.INTAKE_SPEED);
 		instance.intakeLeft.setSpeed(CatzConstants.INTAKE_SPEED);
-
-	}
-	
-	public void stopLauncher() {
-		instance.intakeRight.setSpeed(0);
-		instance.intakeLeft.setSpeed(0);
-		
 	}
 
-	public void retractBicep() { // meant to be used with right bumper
+	public void retractBicep() 
+	{ 
 		CatzConstants.bicepDeployed = false;
 		instance.intakeBicep.set(CatzConstants.bicepDeployed);
-
 	}
 
-	public void deployBicep() { // meant to be used with left bumper
+	public void deployBicep() 
+	{
 		CatzConstants.bicepDeployed = true;
 		instance.intakeBicep.set(CatzConstants.bicepDeployed);
 	}
-
-
 }
